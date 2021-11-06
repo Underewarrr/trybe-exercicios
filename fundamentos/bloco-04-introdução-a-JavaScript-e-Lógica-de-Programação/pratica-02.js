15146f45d140b2eac38d09e2ad39384a0ffc1c44
underewar <rafhaelxd@gmail.com>
2021-11-05 22:56:53 -0300

    boolean expresion added

---
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-02.js
@@ -0,0 +1,8 @@
+/* 
+Faça um programa que retorne o maior de dois números. 
+Defina no começo do programa duas constantes com os valores que serão comparados.
+*/
+const a = 5; //Const defined 
+const b = 15; //Const defined 
+//comparing results
+console.log(a < b);
