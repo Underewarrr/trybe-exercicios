e15517b37dd671fc94341ff0ab22bfc962193a77
underewar <rafhaelxd@gmail.com>
2021-11-05 22:43:20 -0300

    sum added

---
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-01.js
@@ -0,0 +1,16 @@
+/* 
+Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas constantes, a e b ;
+Definidas no começo com os valores que serão operados. 
+Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+
+const a = 2; //Declarada variavel a
+const b = 3; // Declarada variavel b
+    //Soma
+console.log(a + b);
