860556911057191e4eb7e78609aeae99d40c3adf
underewar <rafhaelxd@gmail.com>
2021-11-05 23:30:54 -0300

    aça um programa que, dado um valor definido numa constante

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-05.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-05.js
@@ -3,8 +3,7 @@ Faça um programa que, dado um valor definido numa constante, retorne "positive"
 se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
 */
-const num1 = "xablau";
+cost num1 = 5;
 if (num1 > 0){
-    console.log("Positive") //return positive
-}else if (num1 < 0){console.log("Negative") // return negative
-}else{console.log("zero")} // return zero
+    console.log("Positive")
+}
