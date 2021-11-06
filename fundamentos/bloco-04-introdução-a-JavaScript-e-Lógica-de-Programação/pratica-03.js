d5a3ef658d0d6c71504976c89fde7f7b6c08c746
underewar <rafhaelxd@gmail.com>
2021-11-05 23:14:07 -0300

    fixig js comments /* */

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-03.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/pratica-03.js
@@ -5,8 +5,8 @@
 const num1 = 05;
 const num2 = 15;
 const num3 = 25;
-if (num1 < num2 <= num3){
+if (num1 < num2 <= num3){ // Num1 Wins
    console.log("num1 wins"); 
-}else if(num3 > num2 > num1){
-    console.log("num3 wins");
-}else{console.log("num2 wins");}
+}else if(num3 > num2 > num1){ // Num3 lost
+    console.log("num3 lost");
+}else{console.log("num2 lost");} // num2 lost
