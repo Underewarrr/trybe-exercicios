c3d6803835cd82967a8ce985169a9d018e03d5ab
underewar <rafhaelxd@gmail.com>
2021-11-10 00:09:13 -0300

    fixing files

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/practice-03.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/practice-03.js
@@ -1,19 +1,3 @@
-// EXEMPLO FOR DENTRO DE FOR
-let ingredientes = ["queijo","presunto","maionese"]; 
-let ingredientesAdicionais = true;
-let sacola = [];
-//if (ingredientesAdicionais === true){
-    for(let index = 0; index <= 10; index+=1){
-    console.log('Pão sendo criado fora: ', index)
-            //Criou o pao
-    let pao = [];
-            // Adicionar ingredientes
-    for( let index = 0; index < ingredientes.length; index+=1){
-     console.log('Pão sendo criado exectuou dentro: ', index);
-    pao.push(ingredientes[index])
-   // pao.push("Tomate", "Alface", "Milho")
-    }
-    sacola.push(pao);
-    console.log(sacola)
-    } 
-//}
+/*
+Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+*/
