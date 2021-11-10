f655af9622d9e3f249853977d79a0b43c5549a8f
underewar <rafhaelxd@gmail.com>
2021-11-10 00:07:30 -0300

    new files/modified files

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/pratica-02.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/pratica-02.js
@@ -1,23 +1,22 @@
 /*
 Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 */
-
 /*      Some Documentation
     FINDING SUM IN ARRAY - https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
     WHAT IS REDUCE https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     HOW MAKE A FUNCTION? https://www.w3schools.com/js/js_function_closures.asp
 */
+/* For Example 4.3 */
+/**/
+
 /*
  1 + 2 + 3 + 4
 console.log(array1.reduce(reducer));
 expected output: 10
-
  5 + 1 + 2 + 3 + 4
 console.log(array1.reduce(reducer, 5));
 expected output: 15
-
 */
-
 //This Variable bellow are called in funcion add
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27].reduce(add, 0); // with initial value to avoid when the array is empty
 /*Reduce can call a array and manipuled him with a variale receving value from function in initial arra 0 when add are called
