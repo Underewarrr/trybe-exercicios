a014261f8d9a1b87bdb227d6ddb8d9f9b61a4ecf
underewar <rafhaelxd@gmail.com>
2021-11-13 18:59:16 -0300

    files organized

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/lecture 4.3.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/arrays-loops-for/lecture 4.3.js
@@ -1,20 +1,20 @@
 // EXEMPLO FOR DENTRO DE FOR
 let qtd = 10;
-let ingredientes = ["queijo","presunto","maionese"];
+let ingredientes = ["10","9","8","7","6","4","3","2","1" ];
 let ingredientesAdicionais = true;
 let sacola = [];
-//if (ingredientesAdicionais === true){
+//if (x == 10){
     for(let index = 0; index <= 10; index+=1){
-    console.log('Pão sendo criado fora: ', index)
-            //Criou o pao
+    console.log('Lista De 0 a 10: ', index)
+           
     let pao = [];
-            // Adicionar ingredientes
-    for( let index = 0; index < ingredientes.length; index+=1){
+    
+/*     for( let index = 0; index < ingredientes.length; index+=1){
      console.log('Pão sendo criado exectuou dentro: ', index);
     pao.push(ingredientes[index])
-   // pao.push("Tomate", "Alface", "Milho")
+   pao.push("Tomate", "Alface", "Milho") */
     }
-    sacola.push(pao);
-    console.log(sacola)
-    } 
+   // sacola.push(pao);
+    console.log(ingredientes)
+    
 //}
--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/objetos-e-funcoes/pratica-01.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/objetos-e-funcoes/pratica-01.js
@@ -1,20 +1,22 @@
-var logBackup = console.log;
-var logMessages = [];
-
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    msg: {
-        welcomeMsg : "'Bem Vindo!'"
-    },
-    language:{
-       ptBr:'pt-br'
-    },
-    country:{
-        brazil:'Brasil'
-    },
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-
-  console.log(info.personagem + " received a msg from server", info.msg["welcomeMsg"],  ": from country : " + info.country['brazil'])
+  //console.log(info.personagem + " received a msg from server", info.msg["welcomeMsg"],  ": from country : " + info.country['brazil'])
   /*  Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: */
+  console.log = function() {
+    let info = {
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        msg: {
+            welcomeMsg : "'Bem Vindo!'"
+        },
+        language:{
+        ptBr:'pt-br'
+        },
+        country:{
+            brazil:'Brasil'
+        },
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+   return console.log(info.personagem + " received a msg from server", info.msg["welcomeMsg"],  ": from country : " + info.country['brazil']);
+    //logMessages.push.apply(logMessages, logMessages);
+    //  logBackup.apply(console, logMessages);
+};
