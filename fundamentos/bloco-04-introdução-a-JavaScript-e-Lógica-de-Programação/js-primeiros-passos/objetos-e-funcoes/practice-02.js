7a6c9bb6f13d15eda23a4b9628fed8ad8bcf79e7
underewar <rafhaelxd@gmail.com>
2021-11-18 18:39:02 -0300

    files organized

--- fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/objetos-e-funcoes/practice-02.js
+++ fundamentos/bloco-04-introdução-a-JavaScript-e-Lógica-de-Programação/js-primeiros-passos/objetos-e-funcoes/practice-02.js
@@ -1,12 +1,19 @@
 /* 
-- Insira no objeto uma nova propriedade com o nome de chave 
-'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
-*/
+-Usando o objeto abaixo, faça os exercícios a seguir:
 
-let something = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
-    
+6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , 
+e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+*/
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+console.log("O livro favorito de " + leitor.nome + " ", leitor.sobrenome, "Se chama :" + leitor.livrosFavoritos.titulo);
