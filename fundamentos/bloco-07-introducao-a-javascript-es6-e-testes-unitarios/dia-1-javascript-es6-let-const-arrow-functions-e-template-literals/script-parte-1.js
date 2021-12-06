5e3edef838e004f8469e5c515effc5eff1a95ecf
Underewar <Underewar@gmail.com>
2021-12-06 08:31:17 -0500

    Parte I Using array.sort() to organize an array

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script-parte-1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script-parte-1.js
@@ -4,8 +4,8 @@ Modifique a estrutura da função para que ela seja uma arrow function .
 Modifique as concatenações para template literals .
 Copie o código abaixo. */
 
-//Refactoring to Arrow Functiom
-
+/*  refactoring to Arrow Functiom
+ 
 const testingScope(escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';//var remo
@@ -18,4 +18,16 @@ const testingScope(escopo) => {
     console.log('${ifScope} +  o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
-  testingScope(true);
+  testingScope(true); */
+/*  
+Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+Copie o código abaixo.
+*/
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// Seu código aqui.
+oddsAndEvens.sort(function(a, b) {
+    return a - b;
+  });
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
