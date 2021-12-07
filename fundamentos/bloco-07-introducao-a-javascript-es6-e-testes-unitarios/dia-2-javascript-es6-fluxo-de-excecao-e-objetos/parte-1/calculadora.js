b8363ee1174aeab780e659ed500e7a11c9b29c41
Underewar <Underewar@gmail.com>
2021-12-07 08:01:46 -0500

    compraing with if statement throw new Error

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/calculadora.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-1/calculadora.js
@@ -1,3 +1,22 @@
+/* 1 - Crie um erro personalizado que será chamado se a pessoa usuária digitar apenas um número.
+
+Tente executar a aplicação com um dos valores em branco. Notou que o retorno não é muito descritivo?
+Utilize o throw new Error e o bloco try/catch .
+Imprima o erro no parágrafo com id result , para que a pessoa usuária saiba o que aconteceu. Lembre-se de usar erros descritivos!
+Evite funções que tenham muitas responsabilidades distintas.
+ */
+
+
+/*
+const sum = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
+
+console.log(sum(2, '3'));
+*/
 function sum() {
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
@@ -5,7 +24,12 @@ function sum() {
     document.getElementById('result').innerHTML = `Resultado: ${result}`;
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
-  }
+    if (value1.length >= 0 ||value2.length >= 0) {
+      throw new Error('Insira mais de um numero');
+    }
+   
+  };  
+
   window.onload = () => {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
