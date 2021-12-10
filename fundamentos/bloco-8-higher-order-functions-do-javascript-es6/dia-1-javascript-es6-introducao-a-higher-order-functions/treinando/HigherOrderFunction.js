90bbe40d3f720652d04eb249a07b6bff4759e281
Underewar <Underewar@gmail.com>
2021-12-10 17:00:01 -0500

    bonus finalizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/treinando/HigherOrderFunction.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('#signup-button');
-
-const registerUser = () => {
-  console.log('Registrado com sucesso!');
-};
-
-button.addEventListener('click', registerUser);
-// Lembre-se: First-Class Functions é o nome do conceito que define a forma que a linguagem (no nosso caso JavaScript) trata suas funções, permitindo que sejam suportadas em operações que são usadas em outros tipos (atribuição, retorno, parâmetro), e HOF é uma função que atende ao critério de receber como parâmetro e/ou retornar outra função.
