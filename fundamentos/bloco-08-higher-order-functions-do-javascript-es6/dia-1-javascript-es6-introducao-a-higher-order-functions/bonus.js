fd33a4f9b44b57181229f02482571d10980ff234
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2021-12-10 17:18:08 -0500

    Update bonus.js

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -1,6 +1,6 @@
   const daronLordDmg = (fire) => {
       const minDmg = 20;
       const {fireDmg} = fire
-      const dragonLordDmg = Math.floor((Math.random() * (minDmg - fireDmg + 1) + minDmg));
+      const dragonLordDmg = Math.floor((Math.random() * (fireDmg - minDmg + 1) + minDmg));
         return dragonLordDmg
     }
