5bfb83bc55d28dcb820bc8e6ac7e95bb9089af06
underewar <root@DESKTOP-2OBRKDO.localdomain>
2022-04-07 07:43:43 -0300

    Dev: Fixando 2

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fixando/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/fixando/fancy-buttons/src/App.js
@@ -1,28 +1,34 @@
+import React from 'react';
 import './App.css';
 
+class App extends React.Component {
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
 
 
+  handleClick2() {
+    console.log(this)
+  }
+  handleClick() {
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
+    console.log(this)
+    console.log('Clicou!')
+  }
 
-function Button1() {
-  console.log('Button1')
-}
-
-function Button2() {
-  console.log('Button2')
-}
-
-function Button3() {
-  console.log('Button3')
-}
-
-function App() {
-  return (
-   <>
-   <button onClick={Button1} id="1">Button 1</button>
-   <button onClick={Button2} id="2">Button 2</button>
-   <button onClick={Button3} id="3">Button 3</button>
-   </>
-  );
+  render() {
+    return ( 
+      <>
+    <button onClick={this.handleClick}>Meu botão</button>
+    <button onClick={this.handleClick2}>Meu botão 2</button>
+    </>
+    )
+  }
 }
 
 export default App;
