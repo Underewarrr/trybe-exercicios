5bb5b9345feb2f3d5785cdd6797917994043e6e0
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-27 16:36:52 -0300

    Revert "Dev: Cria IMC"
    This reverts commit 6521d2c0b9b131fb2991e0502b2a428a3c18f315.

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/imc.js
@@ -1,5 +1,7 @@
-let altura = parseFloat(("1.79").replace(",", "."));
-let peso = parseFloat(("68").replace(",", "."));
+let readlineSync = require('readline-sync');
+
+let altura = parseFloat(readlineSync.question("Digite sua altura: "));
+let peso = parseFloat(readlineSync.question("Digite seu peso: "));
 let imc = peso / (altura * altura);
 console.log(`Seu IMC é ${imc}`);
 if (imc < 18.5) {
@@ -8,4 +10,7 @@ if (imc < 18.5) {
 }
 else if (imc >= 18.5 && imc <= 24.9) {
     console.log("Você está no peso ideal");
+}
+else {
+    console.log("Você está acima do peso");
 }
