cd2b50248ca5c7e93247fc90bc55111fef2e3bad
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-27 13:45:33 -0300

    Dev: Cria IMC

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/imc.js
@@ -0,0 +1,11 @@
+let altura = parseFloat(("1.79").replace(",", "."));
+let peso = parseFloat(("68").replace(",", "."));
+let imc = peso / (altura * altura);
+console.log(`Seu IMC é ${imc}`);
+if (imc < 18.5) {
+    
+    console.log("Você está abaixo do peso");
+}
+else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Você está no peso ideal");
+}
