5a43e5012791bb705b6ca29a3c207e3b427fbb8d
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-29 10:16:00 -0300

    Dev: Try read Api from file

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
@@ -1,4 +1,47 @@
-let lines = []
-lineReader.eachLine('./movies.txt', lines.push)
+const axios = require("axios").default
 
-Promise.all(lines.map(getMovie)).then(console.log)
+const lineReader = require('line-reader');
+
+lineReader.eachLine('./movies.txt', async function (line, last, callback) {
+  const movie = await getMovie(line)
+  console.log(movie);
+
+  callback(last);
+});
+
+
+/* const movie = {
+    "id": imdbID,
+    "title": Title,
+    "year": Year + "-01-01",
+    "runtime": Runtime,
+    "genres": Genre,
+    "director": Director,
+    "plot": Plot,
+    "rating": parseFloat(imdbRating),
+  }; */
+
+async function getMovie(title) {
+    try {
+        const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=4a3b6f7a`)
+        const { imdbID, Title, Year, Runtime, Genre, Director, Plot, imdbRating } = data;
+        const movie = {
+            "id": imdbID,
+            "title": Title,
+            "year": Year + "-01-01",
+            "runtime": Runtime,
+            "genres": Genre,
+            "director": Director,
+            "plot": Plot,
+            "rating": parseFloat(imdbRating),
+          };
+        return response.data
+    }
+    catch (error) {
+        return {
+            title: title,
+            error: error.message
+        }
+    }
+}
+
