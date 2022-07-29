9e7f582c1dd836721d4d0f3e09edb5ae56939fa5
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-29 10:22:25 -0300

    Dev: Get full request

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
@@ -23,25 +23,11 @@ lineReader.eachLine('./movies.txt', async function (line, last, callback) {
 
 async function getMovie(title) {
     try {
-        const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=4a3b6f7a`)
-        const { imdbID, Title, Year, Runtime, Genre, Director, Plot, imdbRating } = data;
-        const movie = {
-            "id": imdbID,
-            "title": Title,
-            "year": Year + "-01-01",
-            "runtime": Runtime,
-            "genres": Genre,
-            "director": Director,
-            "plot": Plot,
-            "rating": parseFloat(imdbRating),
-          };
+        const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=4a3b6f6f`)
         return response.data
     }
     catch (error) {
-        return {
-            title: title,
-            error: error.message
-        }
+        console.log(error)
     }
 }
 
