4ec694ad238b96c2c60856773edf4ac36e24d503
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-29 11:29:02 -0300

    Dev: Add Search by newPromise

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/pratica1.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/pratica1.js
@@ -9,17 +9,24 @@ rl = readline('./assets/cep.txt');
 
 rl.on('line', function(line, lineCount, byteCount) {
     // do something with the line of text
-    axios.get(`http://ws.matheuscastiglioni.com.br/ws/cep/find/${line}/json`, (err, res) => {
-    
-    if (res.data.cep) {
-            console.log(res.data);
-            res.status(200).json('Working', res.data.cep);
-            response.json()
-            data => cep = data.cep
-        }
-        else {
-            res.status(404).json('Not found', err);
-        }
-    }
-    )
+
+   
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!resolve) {
+                throw new Error("Deu erro");
+            }
+			resolve("Promise resolvida");
+            console.log("Promise resolvida");
+            axios.get(`http://www.omdbapi.com/?apikey=f63f1ecf&t=${line}`)
+            .then(function(response) {
+                resolve(response.data)
+                console.log(response.data)
+            }
+            ).catch(function(error) {
+        
+            }
+            )
+        }, 5000);
+    });
 })
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
@@ -1,44 +1,32 @@
-  const axios = require("axios").default
+axios = require('axios')
+let readline = require('linebyline'),
+rl = readline('../assets/movie.txt');
 
-  const lineReader = require('line-reader');
-  
-  lineReader.eachLine('./movies.txt', async function (line, last, callback) {
-    const movie = await getMovie(line)
-    console.log(movie);
-    
-    callback(last);
-});
 
-async function getMovie(title) {
-    const apikey = 'f63f1ecf'
-     try {
-        const response = await axios.get(`http://www.omdbapi.com/?apikey=${apikey}&t=${title}`)
-        // http://www.omdbapi.com/?apikey=[yourkey]&t=[movie title]
-        return response.data
-    }
-    catch (error) 
-      {
-        console.log(error)}
-    } {
-    return {
-        title: "Movie not found"
-    }
-  
-  }
-  
-  /* const movie = {
-      "id": imdbID,
-      "title": Title,
-      "year": Year + "-01-01",
-      "runtime": Runtime,
-      "genres": Genre,
-      "director": Director,
-      "plot": Plot,
-      "rating": parseFloat(imdbRating),
-    }; */
-  
-  
-  
-  
-  
-  
+
+
+
+
+rl.on('line', function(line, lineCount, byteCount) {
+    // do something with the line of text
+console.log(line)
+   
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!resolve) {
+                throw new Error("Deu erro");
+            }
+			resolve("Promise resolvida");
+            console.log("Promise resolvida");
+            axios.get(`http://www.omdbapi.com/?apikey=f63f1ecf&t=${line}`)
+            .then(function(response) {
+                resolve(response.data)
+                console.log(response.data)
+            }
+            ).catch(function(error) {
+        
+            }
+            )
+        }, 5000);
+    });
+})
