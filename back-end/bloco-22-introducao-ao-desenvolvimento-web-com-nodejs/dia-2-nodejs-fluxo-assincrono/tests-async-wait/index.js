2829bf8f1a155871fb5b53f0b8b75f53f216e2da
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-29 10:38:14 -0300

    Dev: Readline and search API Working :rocket:

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/tests-async-wait/index.js
@@ -1,33 +1,45 @@
-const axios = require("axios").default
+  const axios = require("axios").default
 
-const lineReader = require('line-reader');
-
-lineReader.eachLine('./movies.txt', async function (line, last, callback) {
-  const movie = await getMovie(line)
-  console.log(movie);
-
-  callback(last);
+  const lineReader = require('line-reader');
+  
+  lineReader.eachLine('./movies.txt', async function (line, last, callback) {
+    const movie = await getMovie(line)
+    console.log(movie);
+    
+    callback(last);
 });
 
-
-/* const movie = {
-    "id": imdbID,
-    "title": Title,
-    "year": Year + "-01-01",
-    "runtime": Runtime,
-    "genres": Genre,
-    "director": Director,
-    "plot": Plot,
-    "rating": parseFloat(imdbRating),
-  }; */
-
 async function getMovie(title) {
-    try {
-        const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=4a3b6f6f`)
-        return response.data
+      const apikey = 'f63f1ecf'
+     try {
+        const response = await axios.get(`http://www.omdbapi.com/?apikey=${apikey}&t=${title}`)
+        // http://www.omdbapi.com/?apikey=[yourkey]&t=[movie title]
+      return response.data
     }
-    catch (error) {
-        console.log(error)
-    }
-}
-
+      catch (
+          error) {
+          console.log(error)
+          }
+       } {
+        return {
+          title: "Movie not found"
+        }
+  
+  }
+  
+  /* const movie = {
+      "id": imdbID,
+      "title": Title,
+      "year": Year + "-01-01",
+      "runtime": Runtime,
+      "genres": Genre,
+      "director": Director,
+      "plot": Plot,
+      "rating": parseFloat(imdbRating),
+    }; */
+  
+  
+  
+  
+  
+  
