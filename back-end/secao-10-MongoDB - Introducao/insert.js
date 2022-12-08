8ddd8e819076e04d17160de0edcd5352ea4dbb4d
Rafhael Oliveira <rafhaelxd@gmail.com>
2022-12-08 15:44:54 -0300

    Dev: MongoDB Introdução

---
+++ back-end/secao-10-MongoDB - Introducao/insert.js
@@ -0,0 +1,13 @@
+use mongo_db
+
+db.products.insertOne(
+
+    {
+        "_id": 100,
+        "productName": "Caixa",
+
+        "price": 20
+
+    }
+
+)
---
+++ back-end/secao-10-MongoDB - Introducao/insertHasMany.js
@@ -0,0 +1,15 @@
+use mongo_db
+
+db.products.insertMany(
+
+    [
+
+        { "productName": "Lapis", "stock": 10, "price": 20, "status":"A"},
+
+        { "productName": "Tesoura", "price": 5, "status": "B" },
+
+        { "productName": "Borracha", "price": 15, "status": "A" }
+
+    ]
+
+)
